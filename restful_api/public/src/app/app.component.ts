44074cccc4dda5237c7fd7f91ca81132912450f1
Michael <vmichaelkirkaldy@live.com>
2018-09-13 09:59:56 -0500

    changes to app.component.ts

--- restful_api/public/src/app/app.component.ts
+++ restful_api/public/src/app/app.component.ts
@@ -5,6 +5,7 @@ import { Component } from '@angular/core';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
+
 export class AppComponent {
-  title = 'public';
+       title = '1st App';
 }
