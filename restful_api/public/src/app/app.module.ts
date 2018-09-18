2054959327fa2ff855107b5895003f81d322499c
Michael <vmichaelkirkaldy@live.com>
2018-09-18 11:21:56 -0500

    Added nested task component

--- restful_api/public/src/app/app.module.ts
+++ restful_api/public/src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { HttpService } from './http.service';
 import { HttpClientModule } from '@angular/common/http';
-import { FormsModule } from '@angular/forms'
+import { FormsModule } from '@angular/forms';
+import { TaskComponent } from './task/task.component'
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TaskComponent
   ],
   imports: [
     BrowserModule,
---
+++ restful_api/public/src/app/task/task.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit, Input } from '@angular/core';
+
+
+@Component({
+  selector: 'app-task',
+  templateUrl: './task.component.html',
+  styleUrls: ['./task.component.css']
+})
+export class TaskComponent implements OnInit {
+	//Input must be import from Angular
+	 @Input() selectedTask:any;
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
